f3da9ec78860c04a111661a55e4c20155c6e662f
Jeetu <jeetuthakur@gmail.com>
2020-05-25 23:21:54 +0530

    Completed module

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -5,9 +5,7 @@ class QuizQuestionButton extends Component {
     render(){
         return(
             <li>
-                <button text = {this.props.button_text}>
-
-                </button>
+                <button>{this.props.button_text}</button>
             </li>
         )
     }
